e9787d493ee30865d592b000060357e07a0c585c
Lior Amar <lior.amar@parallelmachines.com>
2019-04-26 09:52:14 -0700

    fixing flask example to work with sklearn model

--- 01_flask_model_serving/main.py
+++ 01_flask_model_serving/main.py
@@ -1,55 +1,108 @@
 import argparse
 import logging
-import logging
+import json
+
 import numpy as np
-import pickle
+from sklearn.externals import joblib
+
 
 from flask import Flask
-from flask_restplus import Resource, Api
+from flask import make_response
+from flask import request
+from flask import current_app
 
-app = Flask(__name__)
-api = Api(app)
+from flask_classful import FlaskView, route
 
 
-class SklearnRESTfulServing(Resource):
-    JSON_KEY_NAME = "prediction_vector"
+def output_json(data, code, headers=None):
+    """
+    This method is used to serialize the python dict to a json
+    :param data:
+    :param code:
+    :param headers:
+    :return:
+    """
+    content_type = 'application/json'
+    dumped = json.dumps(data)
+    if headers:
+        headers.update({'Content-Type': content_type})
+    else:
+        headers = {'Content-Type': content_type}
+    response = make_response(dumped, code, headers)
+    return response
 
-    def __init__(self):
-        self._logger = logging.getLogger(SklearnRESTfulServing.__name__)
-        self._model = None
-        self._verbose = self._logger.isEnabledFor(logging.DEBUG)
 
-    def load_model(self, model_path):
-        self._logger.info("Model is loading, path: {}".format(model_path))
-        with open(model_path, "rb") as f:
-            self._model = pickle.load(f)
-            if self._verbose:
-                self._logger.debug("Un-pickled model: {}".format(self._model))
-            self._logger.info("Model loaded successfully!")
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
-    @api.route('/predict')
-    def predict(self, url_params, form_params):
-        if SklearnRESTfulServing.JSON_KEY_NAME not in form_params:
-            raise Exception("Unexpected json format for prediction! Missing '{}' key!"
-                            .format(SklearnRESTfulServing.JSON_KEY_NAME))
 
-        if self._verbose:
-            self._logger.debug("predict, url_params: {}, form_params: {}".format(url_params, form_params))
+class PredictView(FlaskView):
+    representations = {'application/json': output_json}
 
-        if not self._model:
-            return 404, {"error": "Model not loaded yet!"}
-        else:
-            if self._verbose:
-                self._logger.debug("type<form_params>: {}\n{}".format(type(form_params), form_params))
-                two_dim_array = np.array([form_params[SklearnRESTfulServing.JSON_KEY_NAME]])
-                self._logger.debug("type(two_dim_array): {}\n{}".format(type(two_dim_array), two_dim_array))
-                result = self._model.predict(two_dim_array)
-                self._logger.debug("result: {}, type: {}".format(result[0], type(result[0])))
-            else:
-                two_dim_array = np.array([form_params[SklearnRESTfulServing.JSON_KEY_NAME]])
-                result = self._model.predict(two_dim_array)
+    def post(self):
+        print("Got predict")
+        print(request.method)
+        model = current_app.config["model"]
+        if model is None:
+            return {'error': "model is not loaded"}
+        print(request)
+        print(request.content_type)
+        content = request.get_json(force=True)
+        if content is None:
+            return {'error': 'content of request is None'}, 404
 
-            return 200, {"result": result[0]}
+        print("Content is: {}".format(content))
+        if "sample" not in content:
+            return {'error': "sample key is not found in content"}
+
+        sample = content["sample"]
+        np_sample = np.asarray(sample).reshape(-1, len(sample))
+        print("Sample: {}".format(np_sample))
+
+        np_pred = model.predict(np_sample)
+        np_pred_prob = model.predict_proba(np_sample)[0]
+
+        list_pred = list(np_pred)
+        list_pred_prob = list(np_pred_prob)
+
+        list_pred = list(map(float, list_pred))
+        list_pred_prob = list(map(float, list_pred_prob))
+
+        print("prediction: {}".format(list_pred))
+        return {'prediction': list_pred, "prediction_probability": list_pred_prob}
+
+    def shutdown(self):
+        shutdown_server()
+        return 'Server shutting down...'
+
+
+def serv_predictions(port, rest_endpoint, input_model):
+    """
+    Main function running the prediction server
+    :param port:
+    :param rest_endpoint:
+    :input_model: Path to model file to load
+    :return:
+    """
+
+    print("Port:      {}".format(port))
+    print("Endpoint:  {}".format(rest_endpoint))
+    print("Model:     {}".format(input_model))
+    print("================")
+
+    # Loading the model
+    loaded_model = joblib.load(input_model)
+
+    app = Flask(__name__)
+    app.config["model"] = loaded_model
+    print("Starting..")
+    PredictView.register(app, route_base=rest_endpoint)
+
+    app.run(host="0.0.0.0", port=port)
+    print("App is done ... exiting")
 
 
 if __name__ == '__main__':
@@ -59,15 +112,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("port", type=int, help="Port to listen on")
     parser.add_argument("model", help="Path of model to load")
+    parser.add_argument("--rest-endpoint", default="/predict/", help="Endpoint to register")
     parser.add_argument("--log-level", choices=log_levels.keys(), default="info", help="Logging level")
-    args = parser.parse_args()
+    options = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)-15s %(levelname)s [%(module)s:%(lineno)d]:  %(message)s')
-    logging.getLogger('mlpiper').setLevel(log_levels[args.log_level])
+    logging.getLogger('mlpiper').setLevel(log_levels[options.log_level])
 
-    app.run(debug=True)
-
-
-
-
+    serv_predictions(options.port, options.rest_endpoint, options.model)
 
