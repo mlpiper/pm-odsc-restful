b74b2cd125830e956b2577b02c5f27061333b965
Lior Amar <lior.amar@parallelmachines.com>
2019-05-04 10:40:28 -0700

    additions of model
    o other fixes.

--- 01_flask_model_serving/run-ab.sh
+++ 01_flask_model_serving/run-ab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #ab -p predict_payload_1.json -T application/json -n 1000 -v 2 http://127.0.0.1:8888/predict/
-ab -p predict_payload_1.json -T application/json -n 1000  http://127.0.0.1:8888/predict/
+ab -p predict_payload_1.json -T application/json -n 1000  http://127.0.0.1:7777/predict/
 
